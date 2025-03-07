fea6a83b62480fdaf63f1a76770d1e95ce494a0a
avkumaran <149909750+avkumaran@users.noreply.github.com>
2025-03-07 15:38:49 -0500

    Ball collection and starting ml training

--- Assets/CubeAgent.cs
+++ Assets/CubeAgent.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 using Unity.MLAgents;
 using Unity.MLAgents.Sensors;
 using Unity.MLAgents.Actuators;
+using NUnit.Framework.Interfaces;
+using UnityEditor.SearchService;
 
 public class CubeAgent : Agent
 {
@@ -16,6 +18,31 @@ public class CubeAgent : Agent
         rBody.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
     }
 
+    private void checkRays() {
+        RayPerceptionSensorComponent3D sensorComponent = GetComponent<RayPerceptionSensorComponent3D>();
+
+        // sensorResults is an array of type RayPerceptionOutput.RayOutput
+        var sensorResults = RayPerceptionSensor.Perceive(sensorComponent.GetRayPerceptionInput(),false).RayOutputs;
+    
+        for (int i = 0; i < sensorResults.Length; i++) {
+            RayPerceptionOutput.RayOutput result = sensorResults[i];
+            if (result.HitTagIndex != -1) {
+                if (result.HitFraction <= 0.02f) {
+                    Destroy(result.HitGameObject);
+                    SetReward(1.0f);
+                }
+            }
+        }
+    }
+
+    private RayPerceptionOutput.RayOutput[] getRayResults() {
+        RayPerceptionSensorComponent3D sensorComponent = GetComponent<RayPerceptionSensorComponent3D>();
+
+        // sensorResults is an array of type RayPerceptionOutput.RayOutput
+        var sensorResults = RayPerceptionSensor.Perceive(sensorComponent.GetRayPerceptionInput(),false).RayOutputs;
+
+        return sensorResults;
+    }   
 
     public override void OnEpisodeBegin()
     {
@@ -26,7 +53,7 @@ public class CubeAgent : Agent
             this.transform.localPosition = new Vector3(0, 0.5f, 0);
         }
 
-        // Move the target to a new spot
+        // Instantiate food clones
         for (int i = 0; i < 10; i++)
         {
             Instantiate(food, new Vector3(Random.Range(-24f, 24f), 0.5f, Random.Range(-24f, 24f)), transform.rotation);
@@ -35,6 +62,22 @@ public class CubeAgent : Agent
 
     public float speed = 5f;
     public float rotationSpeed = 10f; // Adjust for smooth rotation
+
+    public override void CollectObservations(VectorSensor sensor) {
+    
+        // Agent velocity
+        sensor.AddObservation(rBody.linearVelocity.x);
+        sensor.AddObservation(rBody.linearVelocity.z);
+
+        // Agent position
+        sensor.AddObservation(this.transform.localPosition);
+
+        RayPerceptionOutput.RayOutput[] results = getRayResults();
+        for(int i = 0; i < results.Length; i++) {
+                sensor.AddObservation(results[i].EndPositionWorld);
+                sensor.AddObservation(results[i].HitFraction); // distance from ray hit
+        }
+    }
 
     public override void OnActionReceived(ActionBuffers actionBuffers)
     {
@@ -51,19 +94,23 @@ public class CubeAgent : Agent
             transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
         }
 
-        // Rewards
-        //float distanceToTarget = Vector3.Distance(this.transform.localPosition, Target.localPosition);
-
-        // Reached target
-        /*if (distanceToTarget < 1.42f)
-        {
-            SetReward(1.0f);
-            EndEpisode();
-        }*/
+        // CheckRays
+        checkRays(); // manages food collection and rewards 
 
         // Fell off platform
-        else if (this.transform.localPosition.y < 0)
-        {
+        if (this.transform.localPosition.y < 0) {
+            SetReward(-0.5f);
+
+            GameObject[] objects = GameObject.FindGameObjectsWithTag("Food");
+            foreach(GameObject obj in objects) {
+                Destroy(obj);
+            }
+            
+            EndEpisode();
+        }
+
+        // All food collected
+        if (GameObject.FindWithTag("Food") == null) {
             EndEpisode();
         }
     }
